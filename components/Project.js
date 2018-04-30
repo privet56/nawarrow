25a98af7b24170157b24ce06ac51212e7352ef09
privet <privet56@hotmail.com>
2018-04-30 19:04:02 +0200

    opacity for Text

--- components/Project.js
+++ components/Project.js
@@ -8,24 +8,30 @@ const Project = ({ item }) => (
       styles.pr, item.i &&
       styles.incomingPr
     ]}>
-    <Text style={[ styles.prtext ]}>{item.name}</Text>
+    <Text style={[ styles.prtext, styles.prtextname ]}>{item.name}</Text>
     <Text style={[ styles.prtext ]}>{item.description}</Text>
   </View>
 )
-
+//TODO: override opacity for text better as "rgba(180, 249, 119, 1.0)"
 const styles = {
   prtext: {
-    color:"white"
+    color: "rgba(180, 249, 119, 1.0)",
+    fontSize: 20,
+    opacity: 1.0
   },
+  prtextname: {
+    fontWeight: 'bold',
+    fontSize: 30
+  },  
   pr: {
-    width: '70%',
     margin: 10,
     padding: 10,
     backgroundColor: '#e8b566',
     borderColor: '#979797',
     borderStyle: 'solid',
     borderWidth: 1,
-    borderRadius: 10
+    borderRadius: 10,
+    opacity: 0.2
   },
   incomingPr: {
     alignSelf: 'flex-end',
